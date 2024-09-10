caef481483bc44d5d57a2d7b7967b4eb6cbae179
ngocanh8816 <ngocanhnguyenn1018@gmail.com>
2024-09-11 01:36:15 +0700

    Update practice_5.sql

--- practice_5.sql
+++ practice_5.sql
@@ -22,10 +22,57 @@ FROM emails A
 LEFT JOIN texts B ON A.email_id = B.email_id
 
 --EX3:
+SELECT
+age_bucket,
+ROUND(
+SUM(CASE
+  WHEN activity_type = 'send' THEN time_spent
+  ELSE 0
+END)/
+(SUM(CASE
+  WHEN activity_type = 'send' THEN time_spent
+  ELSE 0
+END)+SUM(CASE
+  WHEN activity_type = 'open' THEN time_spent
+  ELSE 0
+END))*100.0,2)
+AS send_perc,
+  
+ROUND(
+SUM(CASE
+  WHEN activity_type = 'open' THEN time_spent
+  ELSE 0
+END)/
+(SUM(CASE
+  WHEN activity_type = 'open' THEN time_spent
+  ELSE 0
+END)+SUM(CASE
+  WHEN activity_type = 'send' THEN time_spent
+  ELSE 0
+END))*100.0,2)
+AS open_perc
+  
+FROM activities A
+JOIN age_breakdown B ON A.user_id = b.user_id
+GROUP BY age_bucket
 
+--HOẶC 
+SELECT
+age_bucket,
+ROUND(SUM(time_spent) FILTER (WHERE activity_type = 'open')/SUM(time_spent)*100.0,2) AS open_perc,
+ROUND(SUM(time_spent) FILTER (WHERE activity_type = 'send')/SUM(time_spent)*100.0,2) AS send_perc
+FROM activities A
+JOIN age_breakdown B ON A.user_id = B.user_id
+WHERE activity_type IN ('open','send')
+GROUP BY age_bucket
 
 --EX4:
-
+SELECT
+customer_id
+FROM products A
+JOIN customer_contracts B ON A.product_id = B.product_id
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_category) = (SELECT COUNT(DISTINCT product_category) FROM products)
 
 --EX5:
 

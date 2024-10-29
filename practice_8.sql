3e1673e037f33840c338b2d7829f20e2b924dc8d
Ngọc Anh <ngocanhnguyenn1018@gmail.com>
2024-10-30 00:24:31 +0700

    Update practice_8.sql

--- practice_8.sql
+++ practice_8.sql
@@ -34,10 +34,48 @@ GROUP BY customer_id
 )
 
 --EX2:
+WITH CTE_A AS
+(
+SELECT
+player_id,
+MIN(event_date)
+FROM Activity 
+GROUP BY player_id
+),
+CTE_B AS
+(
+SELECT *,
+LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS latter_date
+FROM Activity
+)
+SELECT
+ROUND(COUNT(DISTINCT CTE_B.player_id)/COUNT(CTE_A.player_id),2) AS fraction
+FROM CTE_A, CTE_B
+WHERE CTE_B.latter_date - CTE_B.event_date = 1
+--> CÁCH NÀY KHÔNG TỐI ƯU VÌ CÓ THỂ TÍNH LUÔN NHỮNG USER KO ĐĂNG NHẬP VÀO NGÀY TIẾP THEO NGÀY ĐẦU TIÊN MÀ ĐĂNG NHẬP TIẾP VÀO NHỮNG NGÀY SAU ĐÓ
 
+    --CÁCH CODE TỐI ƯU CHO MỌI TRƯỜNG HỢP
+WITH CTE_A AS
+(
+SELECT
+player_id,
+DATE_ADD(MIN(event_date), INTERVAL 1 DAY)
+FROM Activity 
+GROUP BY player_id
+)
+SELECT
+ROUND(COUNT(player_id)/
+(SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS fraction
+FROM Activity
+WHERE (player_id, event_date) IN (SELECT * FROM CTE_A)
 
 --EX3:
-
+SELECT id,
+CASE
+    WHEN id%2 != 0 THEN COALESCE(LEAD(student) OVER (ORDER BY id), student)
+    ELSE LAG(student) OVER (ORDER BY id)
+END AS student
+FROM Seat
 
 --EX4:
 
